d3f70bc7e5671960cc0eb7aeeed08efedda8ecab
Nathasha <nsoares@corpcloud.com.au>
2022-11-24 14:49:07 +0800

    added ReactAnimatedWeather

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
+import ReactAnimatedWeather from "react-animated-weather";
 import Weather from "./WeatherSearch.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
-        <h1>Week 4 Challenge</h1>
-        <Weather city="Perth" />
-      
-    
-<br />
+      <h1>React + GitHub + Netlify Challenge</h1>
 
-    <small>
-      <a href="https://github.com/os-nath/w4-challenge-react" target="blank">Open source </a>created by Nath Soares and hosted 
-      <a href="https://637f0fb6a69c553242656568--precious-dodol-20f065.netlify.app/" target="blank"> here</a>
-    </small>
- </div> 
- );
+      <ReactAnimatedWeather
+        icon="CLEAR_DAY"
+        color="#592693"
+        size={50}
+        animate={true}
+      />
+      <br />
+      <br />
+      <br />
+
+      <Weather city="Perth" />
+
+      <br />
+
+      <small>
+        <a href="https://github.com/os-nath/w4-challenge-react" target="blank">
+          Open source{" "}
+        </a>
+        created by Nath Soares and hosted
+        <a
+          href="https://637f0fb6a69c553242656568--precious-dodol-20f065.netlify.app/"
+          target="blank"
+        >
+          {" "}
+          here
+        </a>
+      </small>
+    </div>
+  );
 }
 
 export default App;
